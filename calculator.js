d3cd42ac24f634d85154740dbd5861f287393463
kiyi238 <79978395+kiyi238@users.noreply.github.com>
2021-03-03 13:25:52 -0400

    Delete calculator.js

--- calculator.js
+++
@@ -1,7 +0,0 @@
-function calculateMwitt() {
-  var femaleBreed = document.getElementById(breed).options[breed.selectedIndex].value;
-
-  var container = document.getElementById("containert");
-  container.appendChild(femaleBreed);
-  container.appendChild(document.createElement("br"));
-}
