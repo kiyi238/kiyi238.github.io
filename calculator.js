504a2c11bbec2bb2a910abab451e4c3a01585e9c
kiyi238 <79978395+kiyi238@users.noreply.github.com>
2021-03-04 11:07:45 -0400

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -1,15 +1,21 @@
 function calculateMwitt() {
-  var femaleMarkingColors = [];
+  var femaleMarkings = [];
+  var femaleMarkingOpacities = [];
 
   var container = document.getElementById("containerf");
-  var inputs = container.getElementsByTagName("input");
-  for (var index = 0; index < inputs.length; ++index) {
-    femaleMarkingColors.push(inputs[index].value);
+  var inputs = container.getElementsByTagName("select");
+  
+  if(inputs[0] != "undefined") {
+    for (var index = 0; index < inputs.length; ++index) {
+      if(inputs[index].name.includes("marking")) {
+        femaleMarkings.push(inputs[index].value);
+      } else { femaleMarkingOpacities.push(inputs[index].value); }
+    }
   }
 
   container = document.getElementById("containert");
-  container.appendChild(document.createTextNode("" + inputs[0].value));
+  container.appendChild(document.createTextNode("" + femaleMarkingOpacities[0]));
   container.appendChild(document.createElement("br"));
-  container.appendChild(document.createTextNode("" + femaleMarkingColors[0]));
+  container.appendChild(document.createTextNode("" + femaleMarkings[0]));
   container.appendChild(document.createElement("br"));
 }
