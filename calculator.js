6712b7c23454c5ef294480b95bc181c5e0188f8b
kiyi238 <79978395+kiyi238@users.noreply.github.com>
2021-03-04 11:52:12 -0400

    Add files via upload

--- calculator.js
+++ calculator.js
@@ -1,15 +1,60 @@
+function Mweor(breed, base, second, tert, con) {
+  this.breed = document.getElementById(breed).value;
+  this.base = document.getElementById(base).value;
+  this.second = document.getElementById(second).value;
+  this.tert = document.getElementById(tert).value;
+  var markings = getMarkings(con);
+  var markingColors = getMarkingColors(con);
+  var markingOpacities = getMarkingOpacities(con);
+}
+
 function calculateMwitt() {
-  var femaleBreed = document.getElementById("breed").value;
-  var femaleBase = document.getElementById("base").value;
-  var femaleSecondary = document.getElementById("secondary").value;
-  var femaleTertiary = document.getElementById("tertiary").value;
-  
-  container = document.getElementById("containert");
-  container.appendChild(document.createTextNode("" + femaleBreed));
+  var femaleMweor = new Mweor("breed","base","secondary","tertiary","containerf");
+  var maleMweor = new Mweor("breedm","basem","secondarym","tertiarym","containerm");
+
+  var container = document.getElementById("containert");
+  container.appendChild(document.createTextNode("" + femaleMweor.breed));
   container.appendChild(document.createElement("br"));
-  container.appendChild(document.createTextNode("" + femaleBase));
-  container.appendChild(document.createElement("br"));
-  container.appendChild(document.createTextNode("" + femaleSecondary));
-  container.appendChild(document.createElement("br"));
-  container.appendChild(document.createTextNode("" + femaleTertiary));
+}
+
+function getMarkings(con) {
+  var markings = [];
+  var container = document.getElementById(con);
+  var inputs = container.getElementsByTagName("select");
+
+  if(inputs[0] != "undefined") {
+    for (var index = 0; index < inputs.length; ++index) {
+      if(inputs[index].name.includes("marking")) {
+        markings.push(inputs[index].value);
+      }
+    }
+  }
+  return markings;
+}
+
+function getMarkingOpacities(con) {
+  var opacities = [];
+  var container = document.getElementById(con);
+  var inputs = container.getElementsByTagName("select");
+
+  if(inputs[0] != "undefined") {
+    for (var index = 0; index < inputs.length; ++index) {
+      if(inputs[index].name.includes("opacity")) {
+        opacities.push(inputs[index].value);
+      }
+    }
+  }
+  return opacities;
+}
+
+function getMarkingColors(con) {
+  var colors = [];
+  var container = document.getElementById(con);
+  var inputs = container.getElementsByTagName("input");
+
+  if(inputs[0] != "undefined") {
+    for (index = 0; index < inputs.length; ++index) {
+      colors.push(inputs[index].value);
+    }
+  }
 }
