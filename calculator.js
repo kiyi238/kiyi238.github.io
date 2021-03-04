b1e2a59550ff031a016bf52892caa6197d8bbeae
kiyi238 <79978395+kiyi238@users.noreply.github.com>
2021-03-04 09:46:40 -0400

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -1,6 +1,6 @@
 function calculateMwitt() {
   var femaleBreed = document.getElementById("breed");
-  var option = femaleBreed.options[femaleBreed.selectedIndex].value;
+  var option = femaleBreed.options[femaleBreed.selectedIndex].text;
 
   container = document.getElementById("containert");
   container.appendChild(document.createTextNode("" + femaleBreed));
