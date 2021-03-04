a897321a2ced29660187d8412301965969b7e74e
kiyi238 <79978395+kiyi238@users.noreply.github.com>
2021-03-04 11:10:05 -0400

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -1,21 +1,15 @@
 function calculateMwitt() {
-  var femaleMarkings = [];
-  var femaleMarkingOpacities = [];
-
-  var container = document.getElementById("containerf");
-  var inputs = container.getElementsByTagName("select");
+  var femaleBreed = document.getElementById("breed").value;
+  var femaleBase = document.getElementById("base").value;
+  var femaleSecondary = document.getElementById("secondary").value;
+  var femaleTertiary = document.getElementById("tertiary").value;
   
-  if(inputs[0] != "undefined") {
-    for (var index = 0; index < inputs.length; ++index) {
-      if(inputs[index].name.includes("marking")) {
-        femaleMarkings.push(inputs[index].value);
-      } else { femaleMarkingOpacities.push(inputs[index].value); }
-    }
-  }
-
   container = document.getElementById("containert");
-  container.appendChild(document.createTextNode("" + femaleMarkingOpacities[0]));
+  container.appendChild(document.createTextNode("" + femaleBreed));
   container.appendChild(document.createElement("br"));
-  container.appendChild(document.createTextNode("" + femaleMarkings[0]));
+  container.appendChild(document.createTextNode("" + femaleBase));
   container.appendChild(document.createElement("br"));
+  container.appendChild(document.createTextNode("" + femaleSecondary));
+  container.appendChild(document.createElement("br"));
+  container.appendChild(document.createTextNode("" + femaleTertiary));
 }
