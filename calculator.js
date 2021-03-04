135baeeb0fc00d2ccef80d50cd7785338178c73b
kiyi238 <79978395+kiyi238@users.noreply.github.com>
2021-03-04 09:45:51 -0400

    Add files via upload

--- calculator.js
+++ calculator.js
@@ -1,5 +1,6 @@
 function calculateMwitt() {
-  var femaleBreed = document.getElementById(breed).options[breed.selectedIndex].value;
+  var femaleBreed = document.getElementById("breed");
+  var option = femaleBreed.options[femaleBreed.selectedIndex].value;
 
   container = document.getElementById("containert");
   container.appendChild(document.createTextNode("" + femaleBreed));
