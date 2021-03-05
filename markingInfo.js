d24adf06fd242b679c6ee70832157f1aa9fba0e0
kiyi238 <79978395+kiyi238@users.noreply.github.com>
2021-03-04 20:42:44 -0400

    Delete markingInfo.js

--- markingInfo.js
+++
@@ -1,8 +0,0 @@
-function initializeRecessives() {
-  var recessives = ["Bat Wings","Butterfly Wings","Candleflame Tabby","Classic Tabby",
-                    "Clouded Leopard","Deer Antlers","Dragon Horns","Ear Tufts","Eastern Dragon",
-                    "Elk Horns","Fairy Wings","Feather Wings","Giraffe","King Cheetah","Mackerel Tabby",
-                    "Mane","Marbled","Neck Spikes","Ocelot","Pronghorns","Ram Horns","Saber Fangs",
-                    "Snow Leopard","Spotted Tabby","Unicorn Horn"];
-  return recessives;
-}
